537c0e0e2fc76a4752c36cee2efeed76ac8afd29
Sarah Harvey <sharvey@metaether.net>
2017-02-08 21:13:43 -0800

    Fix indentation because my IDE is nonsense

--- src/Util.java
+++ src/Util.java
@@ -1,17 +1,17 @@
 public class Util {
-  public static byte[] hex2bytes(String hex) {
-    byte[] result = new byte[hex.length()/2];
-    for (int i = 0; i < result.length; i++) {
-      result[i] = (byte)Integer.parseInt(hex.substring(i*2, i*2+2), 16);
+    public static byte[] hex2bytes(String hex) {
+        byte[] result = new byte[hex.length()/2];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (byte)Integer.parseInt(hex.substring(i*2, i*2+2), 16);
+        }
+        return result;
     }
-    return result;
-  }
 
-  public static String bytes2hex(byte[] bytes) {
-    String result = "";
-    for (int i = 0; i < bytes.length; i++) {
-      result += String.format("%02x", bytes[i]);
+    public static String bytes2hex(byte[] bytes) {
+        String result = "";
+        for (int i = 0; i < bytes.length; i++) {
+            result += String.format("%02x", bytes[i]);
+        }
+        return result;
     }
-    return result;
-  }
 }
