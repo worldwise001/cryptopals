cafd934b0db39fde72ab94eef91bc5b7f36796cc
Sarah Harvey <sharvey@metaether.net>
2017-02-08 20:41:40 -0800

    Implement AES decrypt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
   public static void main(String[] args) {
-      byte[] bytes = AES.encrypt("marine sub green".getBytes(), "YELLOW SUBMARINE".getBytes());
+      // 22 b7 68 9e 59 c8 e2 9a 25 01 35 ad fd 50 85 21
+      byte[] key = "YELLOW SUBMARINE".getBytes();
+      byte[] bytes = AES.encrypt("marine sub green".getBytes(), key);
       for (int i = 0; i < bytes.length; i++) {
           System.out.printf("%02x ", bytes[i]);
       }
+      System.out.println();
+      byte[] decrypted = AES.decrypt(bytes, key);
+      System.out.println(new String(decrypted));
   }
 }
