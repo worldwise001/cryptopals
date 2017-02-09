32a307766c76d5e70d3a024a426c5c63e01c660f
Sarah Harvey <sharvey@metaether.net>
2017-02-08 21:35:19 -0800

    Implement challenge 9

---
+++ src/Set2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Set2 {
+    // Challenge 9
+    public static byte[] pkcs7pad(byte[] data, int length) {
+        byte[] result = Arrays.copyOf(data, length);
+        byte pad = (byte)(length - data.length);
+        for (int i = 0; i < pad; i++) {
+            result[result.length-1-i] = pad;
+        }
+        return result;
+    }
+}
